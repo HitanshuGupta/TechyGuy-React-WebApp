79ac9807e1b2fadb5b920fdd64beed43fab98985
hitanshu gupta <hitanshugupta212@gmail.com>
2023-08-28 04:37:01 +0530

    add assets

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  <div className="home">
+    <main>
+        <h1>
+            TechyGuy
+        </h1>
+        <p>Solution of Your all Problems</p>
+    </main>
+  </div>
+  );
+}
+
+export default Home
