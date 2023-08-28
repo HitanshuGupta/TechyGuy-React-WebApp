02938ca1c8c5514d92a28947e5a39eb397a8994b
hitanshu gupta <hitanshugupta212@gmail.com>
2023-08-28 05:44:43 +0530

    Update Brand

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import vg from "../assets/2.webp"
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai"
 
 const Home = () => {
   return (
@@ -13,7 +14,6 @@ const Home = () => {
 
 			<div className="home2">
 				<img src={vg} alt="Graphics" />
-
 				<div>
 					<p>
 						We are your one and only solution to the tech problems you face
@@ -23,15 +23,64 @@ const Home = () => {
 				</div>
 			</div>
 
-            <div className="home3">
+			<div className="home3">
+				<div>
+					<h1>Who we are?</h1>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+						harum quae vitae quos temporibus rem reprehenderit magni tempore
+						asperiores excepturi possimus impedit consectetur neque aliquam
+						veniam accusamus labore commodi voluptatibus quam, consequuntur
+						maxime aperiam expedita. Sit iusto delectus dolores aspernatur
+						tempora animi, repellat saepe hic consequuntur iste id error
+						nesciunt!
+					</p>
+				</div>
+			</div>
 
-                <div>
-                    <h1>Who we are?</h1>
-                    <p>
-                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus harum quae vitae quos temporibus rem reprehenderit magni tempore asperiores excepturi possimus impedit consectetur neque aliquam veniam accusamus labore commodi voluptatibus quam, consequuntur maxime aperiam expedita. Sit iusto delectus dolores aspernatur tempora animi, repellat saepe hic consequuntur iste id error nesciunt!   
-                    </p>
-                </div>
-            </div>
+			<div className="home4">
+				<div>
+					<h1>Brands</h1>
+
+					<article>
+						<div
+							style={{
+								animationDelay: "0.3s",
+							}}
+						>
+							<AiFillGoogleCircle />
+							<p>Google</p>
+						</div>
+
+						<div
+							style={{
+								animationDelay: "0.5s",
+							}}
+						>
+							<AiFillAmazonCircle />
+							<p>Amazon</p>
+						</div>
+
+						<div
+							style={{
+								animationDelay: "0.7s",
+							}}
+						>
+							<AiFillYoutube />
+							<p>Youtube</p>
+						</div>
+
+						<div
+							style={{
+								animationDelay: "1s",
+							}}
+						>
+							<AiFillInstagram />
+							<p>Instagram</p>
+						</div>
+					</article>
+				</div>
+			</div>
 		</>
 	);
 }
