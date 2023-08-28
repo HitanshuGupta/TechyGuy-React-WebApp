d44651dd41ad6f3e40cb9a393fe3b22e449593e5
hitanshu gupta <hitanshugupta212@gmail.com>
2023-08-28 07:03:28 +0530

    Footer and bookmars

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+		<footer>
+			<div>
+				<h1>TechyGuy</h1>
+				<p>@all right Reversed</p>
+			</div>
+
+			<div>
+				<h5>Follow Us</h5>
+				<div>
+					<a href="https://github.com/HitanshuGupta" target={'blank'}>Github</a>
+					<a href="https://www.linkedin.com/in/hitanshu-gupta/" target={'blank'}>Linkedin</a>
+					<a href="https://hitanshu-portfolio.netlify.app/" target={'blank'}>Portfolio</a>
+				</div>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} fro
 const Home = () => {
   return (
 		<>
-			<div className="home">
+			<div className="home" id="home">
 				<main>
 					<h1>TechyGuy</h1>
 					<p>Solution of Your all Problems</p>
@@ -23,7 +23,7 @@ const Home = () => {
 				</div>
 			</div>
 
-			<div className="home3">
+			<div className="home3" id="about">
 				<div>
 					<h1>Who we are?</h1>
 					<p>
@@ -38,7 +38,7 @@ const Home = () => {
 				</div>
 			</div>
 
-			<div className="home4">
+			<div className="home4" id="brands">
 				<div>
 					<h1>Brands</h1>
 
