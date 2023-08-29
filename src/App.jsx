e280d485b2f2a179a70c7b2b893d5ac45be4e4ac
hitanshu gupta <hitanshugupta212@gmail.com>
2023-08-30 01:56:28 +0530

    Adding media queries

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
 import Contact from "./components/Contact.jsx"
+import Services from "./components/Services.jsx";
 
 import "./styles/App.scss"
 import "./styles/Header.scss"
@@ -10,17 +11,19 @@ import "./styles/Home.scss"
 import "./styles/Footer.scss"
 import "./styles/Contact.scss"
 
+
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-      </Router>
-  );
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/services" element={<Services />} />
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
